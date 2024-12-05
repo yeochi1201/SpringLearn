f78d13669885c9618af579473b5d16f5a5289abb
박주현 <yeochi1201@gmail.com>
2024-12-06 04:11:58 +0900

    feature/controller+html

--- SpringLearning/src/main/java/SpringLearning/SpringLearning/SpringLearningApplication.java
+++ SpringLearning/src/main/java/SpringLearning/SpringLearning/SpringLearningApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringLearningApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringLearningApplication.class, args);
 	}
-
 }
