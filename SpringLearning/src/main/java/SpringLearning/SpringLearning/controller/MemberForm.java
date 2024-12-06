3f094e3ba26bfb94eba81ede2d41054c1da15694
박주현 <yeochi1201@gmail.com>
2024-12-06 09:04:07 +0900

    feature/Member Web MVC

---
+++ SpringLearning/src/main/java/SpringLearning/SpringLearning/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package SpringLearning.SpringLearning.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
